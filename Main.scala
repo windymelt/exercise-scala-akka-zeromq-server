acc26a156a6f5db0e97099da819bfebcb3c1fed9
Momijikawa <windy.imlovinit@gmail.com>
2015-06-07 22:57:46 +0900

    Modified to work with Rep-Req pattern

--- Main.scala
+++ Main.scala
@@ -5,28 +5,28 @@ import scala.concurrent.ExecutionContext
 object Main extends App {
   import akka.actor._
   import akka.zeromq._
-  import akka.util.ByteString
-  import concurrent.duration._
-  import collection.immutable.Seq
+
+  class RepActor extends Actor with ActorLogging {
+    import log.{ debug, warning }
+
+    def receive = {
+      case m: ZMQMessage =>
+        debug(s"message inbound: ${m.frames}")
+        sender ! ZMQMessage(m.frame(0), m.frame(1).mapI(_ + 1))
+
+      case unknown => warning("Unknown: $unknown")
+    }
+  }
 
   override def main(args: Array[String]): Unit = {
     val system = ActorSystem("Exercise-ZMQ-Server")
     implicit def execContext: ExecutionContext = system.dispatcher
+    val listener = system.actorOf(Props[RepActor], "repActor")
 
-    val schedule_initialDuration = 1 second
-    val schedule_interval = 2 seconds
-    val topic: String = "ping"
-    val message = "PINGING MESSAGE"
-
-    val pubSocket = ZeroMQExtension(system).newSocket(SocketType.Pub,
+    val repSocket = ZeroMQExtension(system).newSocket(SocketType.Rep,
+      Listener(listener),
       Bind("tcp://127.0.0.1:21231"))
 
     println("test start on\ntcp://127.0.0.1:21231")
-
-    system.scheduler.schedule(schedule_initialDuration, schedule_interval, pubSocket,
-      ZMQMessage(
-        Seq(
-          ByteString(topic), // 第一フレーム目にtopicを指定する
-          ByteString(message))))
   }
 }
